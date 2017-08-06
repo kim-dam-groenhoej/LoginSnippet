4c52bf8e52b57240e7c775a1d4acd820b4e978f0
Kim Dam Grønhøj <kidg@techcollege.dk>
2017-08-07 00:23:02 +0200

    Add project files.

---
+++ LoginSnippet/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LoginSnippet.Startup))]
+namespace LoginSnippet
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
