3533d9f5ed103052885bda55acbef70ab7ed6623
Kim Dam Grønhøj <kidg@techcollege.dk>
2017-08-08 12:28:39 +0200

    Updated role

---
+++ LoginSnippet/Controllers/RolesController.cs
@@ -0,0 +1,29 @@
+﻿using LoginSnippet.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoginSnippet.Controllers
+{
+    public class RolesController : Controller
+    {
+        // GET: Roles
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CreateRole(IdentityRole role)
+        {
+            var context = new ApplicationDbContext();
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            roleManager.Create(role);
+
+            return View();
+        }
+    }
+}
